db8779a98a44bb9e2ea35af6407c77d35651c9b7
DaniellTa <61436073+DaniellTa@users.noreply.github.com>
2024-08-03 00:30:05 -0400

    Update text_to_move.py

--- text_to_move.py
+++ text_to_move.py
@@ -3,44 +3,44 @@ def text_to_move(text: str) -> str:
     move_to_letter = {
         "R": "e",
         "R'": "a",
-        "U": "r",
-        "U'": "i",
+        "B": "r",
+        "B'": "i",
         "L": "o",
         "L'": "t",
         "F": "n",
         "F'": "s",
         "D'": "\n",
         "D": " ",
-        "B'": 1,
-        "B": -1,
+        "U'": 1,
+        "U": -1,
     }
     move_to_letter_left = {
         "R": "u",
         "R'": "g",
-        "U": "h",
-        "U'": "m",
+        "B": "h",
+        "B'": "m",
         "L": "p",
         "L'": "l",
         "F": "c",
         "F'": "q",
         "D'": "\n",
         "D": " ",
-        "B'": 0,
-        "B": "z",
+        "U'": 0,
+        "U": "z",
     }
     move_to_letter_right = {
         "R": "d",
         "R'": "b",
-        "U": "k",
-        "U'": "x",
+        "B": "k",
+        "B'": "x",
         "L": "v",
         "L'": "w",
         "F": "y",
         "F'": "f",
         "D'": "\n",
         "D": " ",
-        "B'": "j",
-        "B": 0,
+        "U'": "j",
+        "U": 0,
     }
     state = 0
     for letter in text:
@@ -49,30 +49,30 @@ def text_to_move(text: str) -> str:
                 res += get_key(letter, move_to_letter) + " "
             elif get_key(letter, move_to_letter_left):
                 state = -1
-                res += "B " + get_key(letter, move_to_letter_left) + " "
+                res += "U " + get_key(letter, move_to_letter_left) + " "
             else:
                 state = 1
-                res += "B' " + get_key(letter, move_to_letter_right) + " "
+                res += "U' " + get_key(letter, move_to_letter_right) + " "
 
         elif state == -1:
             if get_key(letter, move_to_letter_left):
                 res += get_key(letter, move_to_letter_left) + " "
             elif get_key(letter, move_to_letter):
-                res += "B' " + get_key(letter, move_to_letter) + " "
+                res += "U' " + get_key(letter, move_to_letter) + " "
                 state = 0
             else:
                 state = 1
-                res += "B2' " + get_key(letter, move_to_letter_right) + " "
+                res += "U2' " + get_key(letter, move_to_letter_right) + " "
 
         elif state == 1:
             if get_key(letter, move_to_letter_right):
                 res += get_key(letter, move_to_letter_right) + " "
             elif get_key(letter, move_to_letter):
                 state = 0
-                res += "B " + get_key(letter, move_to_letter) + " "
+                res += "U " + get_key(letter, move_to_letter) + " "
             else:
                 state = -1
-                res += "B2 " + get_key(letter, move_to_letter_left) + " "
+                res += "U2 " + get_key(letter, move_to_letter_left) + " "
 
     return res + "D'"
 
