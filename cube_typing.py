a6e87828d74db35370076c62df8ded7b271ba6d4
DaniellTa <61436073+DaniellTa@users.noreply.github.com>
2024-08-03 00:32:41 -0400

    Update cube_typing.py

--- cube_typing.py
+++ cube_typing.py
@@ -23,16 +23,16 @@ def replace_move(move: str):
     move_to_letter = {
         "R": "e",
         "R'": "a",
-        "U": "r",
-        "U'": "i",
+        "B": "r",
+        "B'": "i",
         "L": "o",
         "L'": "t",
         "F": "n",
         "F'": "s",
         "D'": Key.enter,
         "D": Key.space,
-        "B'": 1,
-        "B": -1,
+        "U'": 1,
+        "U": -1,
     }
     return move_to_letter[move]
 
@@ -40,16 +40,16 @@ def replace_move_left(move: str):
     move_to_letter_left = {
         "R": "u",
         "R'": "g",
-        "U": "h",
-        "U'": "m",
+        "B": "h",
+        "B'": "m",
         "L": "p",
         "L'": "l",
         "F": "c",
         "F'": "q",
         "D'": Key.enter,
         "D": Key.space,
-        "B'": 0,
-        "B": "z",
+        "U'": 0,
+        "U": "z",
     }
     return move_to_letter_left[move]
 
@@ -57,16 +57,16 @@ def replace_move_right(move: str):
     move_to_letter_right = {
         "R": "d",
         "R'": "b",
-        "U": "k",
-        "U'": "x",
+        "B": "k",
+        "B'": "x",
         "L": "v",
         "L'": "w",
         "F": "y",
         "F'": "f",
         "D'": Key.enter,
         "D": Key.space,
-        "B'": "j",
-        "B": 0,
+        "U'": "j",
+        "U": 0,
     }
     return move_to_letter_right[move]
 
@@ -92,6 +92,9 @@ def release_arrow(move: str):
     return arrows[move]
 
 
+def inverse_move(move: str):
+    return f"{move}'" if "'" not in move else f"{move}".replace("'", "")
+
 opt = Options()
 opt.add_argument("--window-size=950,1100")
 driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=opt)
@@ -103,16 +106,32 @@ keyboard = Controller()
 check = ""
 is_moving = False
 state = 0
+stack_of_algs = ['']
 while True:
-    link = driver.find_element(By.XPATH,
-        "/html/body/div/div/a").get_attribute("href")
+    link = driver.find_element(By.XPATH, "/html/body/div/div/a").text
+
     if link != check:
         check = link
 
         try:
-            last = link.rfind("=") + 1
-            turn = link[last:].replace("-", "'")
-            
+            if 'moves' not in check:
+                last_letter = check.split(" ")[-1]
+                prev_last_letter = stack_of_algs[-1].split(" ")[-1]
+
+                if len(check.split(" ")) < len(stack_of_algs[-1].split(" ")):
+                    if last_letter == prev_last_letter:
+                        turn = inverse_move(stack_of_algs[-1].split(" ")[-2])
+                    else:
+                        turn = inverse_move(prev_last_letter)
+                elif "2" in last_letter:
+                    turn = last_letter.replace("2", "")
+                elif "2" in prev_last_letter and last_letter in prev_last_letter:
+                    turn = inverse_move(last_letter)
+                else:
+                    turn = last_letter
+                stack_of_algs.append(check)
+
+
             if state == -1:
                 if isinstance(replace_move_left(turn), int):
                     state = replace_move_left(turn)
@@ -132,14 +151,5 @@ while True:
                     keyboard.press(replace_move_right(turn))
                     if replace_move_right(turn) == Key.enter:
                         state = 0
-            # if not is_moving:
-            #     keyboard.press(press_arrow(turn))
-            #     is_moving = True
-            # else:
-            #     keyboard.release(release_arrow(turn))
-            #     is_moving = False
-            driver.find_element(By.XPATH,
-                '/html/body/div/div/button[1]').click()
         except:
-            driver.find_element(By.XPATH,
-                '/html/body/div/div/button[1]').click()
+            print("something didn't work")
